6eac43fea075f74d2b744c5022c343e02d8debc6
hongxi <hongxi@umich.edu>
2021-01-26 20:02:42 -0500

    init

---
+++ cmake-build-debug/CMakeFiles/scHiCEigen.dir/DependInfo.cmake
@@ -0,0 +1,33 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/bj/CLionProjects/EigenscHiCtools/embedding/embedding.cpp" "/Users/bj/CLionProjects/EigenscHiCtools/cmake-build-debug/CMakeFiles/scHiCEigen.dir/embedding/embedding.cpp.o"
+  "/Users/bj/CLionProjects/EigenscHiCtools/embedding/reproducibility.cpp" "/Users/bj/CLionProjects/EigenscHiCtools/cmake-build-debug/CMakeFiles/scHiCEigen.dir/embedding/reproducibility.cpp.o"
+  "/Users/bj/CLionProjects/EigenscHiCtools/load/ContactMaps.cpp" "/Users/bj/CLionProjects/EigenscHiCtools/cmake-build-debug/CMakeFiles/scHiCEigen.dir/load/ContactMaps.cpp.o"
+  "/Users/bj/CLionProjects/EigenscHiCtools/load/load_hic_file.cpp" "/Users/bj/CLionProjects/EigenscHiCtools/cmake-build-debug/CMakeFiles/scHiCEigen.dir/load/load_hic_file.cpp.o"
+  "/Users/bj/CLionProjects/EigenscHiCtools/load/processing_utils.cpp" "/Users/bj/CLionProjects/EigenscHiCtools/cmake-build-debug/CMakeFiles/scHiCEigen.dir/load/processing_utils.cpp.o"
+  "/Users/bj/CLionProjects/EigenscHiCtools/test/test_all.cpp" "/Users/bj/CLionProjects/EigenscHiCtools/cmake-build-debug/CMakeFiles/scHiCEigen.dir/test/test_all.cpp.o"
+  "/Users/bj/CLionProjects/EigenscHiCtools/test/test_reproducibility.cpp" "/Users/bj/CLionProjects/EigenscHiCtools/cmake-build-debug/CMakeFiles/scHiCEigen.dir/test/test_reproducibility.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "HAVE_CBLAS=1"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/Users/bj/opt/anaconda3/include"
+  "/usr/local/include/eigen3"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/scHiCEigen.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/scHiCEigen.dir/embedding/embedding.cpp.o"
+  "CMakeFiles/scHiCEigen.dir/embedding/reproducibility.cpp.o"
+  "CMakeFiles/scHiCEigen.dir/load/ContactMaps.cpp.o"
+  "CMakeFiles/scHiCEigen.dir/load/load_hic_file.cpp.o"
+  "CMakeFiles/scHiCEigen.dir/load/processing_utils.cpp.o"
+  "CMakeFiles/scHiCEigen.dir/test/test_all.cpp.o"
+  "CMakeFiles/scHiCEigen.dir/test/test_reproducibility.cpp.o"
+  "scHiCEigen"
+  "scHiCEigen.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/scHiCEigen.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
